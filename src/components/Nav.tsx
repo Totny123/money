604ddb4302d3f95ae0b400ce2571cde1443691b7
totny <24455370@qq.com>
2021-12-20 16:56:29 +0800

    修复Nav点击区域过小问题及添加活动路由高亮CSS

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,24 +1,30 @@
 import styled from "styled-components";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import Icon from "components/Icon";
 
 const NavWrapper = styled.nav`
   line-height: 20px;
   box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
-
   > ul {
     display: flex;
     text-align: center;
-
     > li {
-      display: flex;
-      flex-direction: column;
-      align-items: center;
       width: 33.33%;
-      padding: 6px 0;
-      .icon {
-        width: 24px;
-        height: 24px;
+      > a {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        padding: 6px 0;
+        .icon {
+          width: 24px;
+          height: 24px;
+        }
+        &.selected {
+          color: red;
+          > .icon {
+            fill: red;
+          }
+        }
       }
     }
   }
@@ -29,16 +35,22 @@ function Nav() {
     <NavWrapper>
       <ul>
         <li>
-          <Icon name="tag" />
-          <Link to="/tags">标签</Link>
+          <NavLink to="/tags" activeClassName="selected">
+            <Icon name="tag" />
+            标签
+          </NavLink>
         </li>
         <li>
-          <Icon name="money" />
-          <Link to="/money">记一笔</Link>
+          <NavLink to="/money" activeClassName="selected">
+            <Icon name="money" />
+            记一笔
+          </NavLink>
         </li>
         <li>
-          <Icon name="chart" />
-          <Link to="/statistics">统计</Link>
+          <NavLink to="/statistics" activeClassName="selected">
+            <Icon name="chart" />
+            统计
+          </NavLink>
         </li>
       </ul>
     </NavWrapper>
