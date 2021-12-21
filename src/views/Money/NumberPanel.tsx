d55728fa849f04d52518a8b997d123dbcfcf2ae0
totny <24455370@qq.com>
2021-12-21 14:56:21 +0800

    Money组件中数字面板功能部分完成

--- src/views/Money/NumberPanel.tsx
+++ src/views/Money/NumberPanel.tsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
-
-const NumberPanel = styled.section`
+import { useState } from "react";
+const Wrapper = styled.section`
   display: flex;
   flex-direction: column;
   .output {
@@ -62,4 +62,61 @@ const NumberPanel = styled.section`
   }
 `;
 
+const NumberPanel = () => {
+  const [output, setOutput] = useState<string>("0");
+  const buttonsClick = (e: React.MouseEvent) => {
+    const text = (e.target as HTMLButtonElement).textContent;
+    if (text === null) return;
+    if (["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"].indexOf(text) > -1) {
+      if (output === "0") {
+        setOutput(text);
+      } else {
+        setOutput((o) => o + text);
+      }
+    }
+    switch (text) {
+      case ".":
+        if (output.indexOf(".") === -1) {
+          setOutput((o) => o + text);
+        }
+        break;
+      case "删除":
+        if (output !== "0") {
+          if (output.length === 1) {
+            setOutput("0");
+          } else {
+            setOutput((o) => o.slice(0, o.length - 1));
+          }
+        }
+        break;
+      case "清空":
+        setOutput("0");
+        break;
+      case "OK":
+        console.log("OK");
+        break;
+    }
+  };
+  return (
+    <Wrapper>
+      <div className="output">{output}</div>
+      <div className="number-buttons clearfix" onClick={buttonsClick}>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>删除</button>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button>清空</button>
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+        <button className="ok">OK</button>
+        <button className="zero">0</button>
+        <button>.</button>
+      </div>
+    </Wrapper>
+  );
+};
 export { NumberPanel };
