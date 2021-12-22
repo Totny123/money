666e431191a9fac172653d284d3e4616bf95f4a4
totny <24455370@qq.com>
2021-12-22 14:18:48 +0800

    Tags组件样式完成

--- src/views/Tags.tsx
+++ src/views/Tags.tsx
@@ -1,15 +1,53 @@
+import Icon from "components/Icon";
 import Layout from "components/Layout";
+import styled from "styled-components";
 import { useTags } from "useTags";
 
 function Tags() {
   const { tags, setTags } = useTags();
+  const TagsList = styled.ul`
+    background: #ffffff;
+    > li {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      padding: 12px 0;
+      margin: 0 16px;
+      font-size: 16px;
+      border-bottom: 1px solid #bcbbc1;
+    }
+  `;
+  const Button = styled.button`
+    padding: 8px 13px;
+    font-size: 18px;
+    background: #767676;
+    border-radius: 4px;
+    border: none;
+    color: white;
+  `;
+  const Center = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  `;
+  const Space = styled.div`
+    height: 44px;
+  `;
   return (
     <Layout>
-      <ul>
+      <TagsList>
         {tags.map((tag) => (
-          <li key={tag}>{tag}</li>
+          <li key={tag}>
+            <span className="one-line">{tag}</span>
+            <Icon name="right"></Icon>
+          </li>
         ))}
-      </ul>
+      </TagsList>
+      <Center>
+        <Space />
+        <Button>新建标签</Button>
+      </Center>
     </Layout>
   );
 }
