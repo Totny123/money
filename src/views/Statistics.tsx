5a36c4ce690fe02b75e990e6ab465f85efd6195b
totny <24455370@qq.com>
2021-12-24 17:48:28 +0800

    统计页样式完成

--- src/views/Statistics.tsx
+++ src/views/Statistics.tsx
@@ -10,6 +10,19 @@ const TypesWrapper = styled.div`
   background: white;
 `;
 
+const Item = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 10px 16px;
+  font-size: 18px;
+  background: white;
+  > .remarks {
+    margin-right: auto;
+    margin-left: 15px;
+    color: #999999;
+  }
+`;
+
 function Statistics() {
   const [type, setType] = useState<"-" | "+">("-");
   const { records } = useRecords();
@@ -25,17 +38,20 @@ function Statistics() {
         />
       </TypesWrapper>
 
-      {records.map((r) => (
-        <div key={r.createdTime}>
-          {r.tagIds.map((tagId) => (
-            <span>{getName(tagId)}</span>
-          ))}
-          <hr />
-          {r.amount}
-          <hr />
-          {day(r.createdTime).format("YYYY年MM月DD日")}
-        </div>
-      ))}
+      {records
+        .filter((re) => re.type === type)
+        .map((r) => (
+          <Item key={r.createdTime}>
+            <div className="tags">
+              {r.tagIds.map((tagId) => (
+                <span>{getName(tagId)}</span>
+              ))}
+            </div>
+            <div className="remarks">{r.remarks}</div>
+            <div className="amount">￥{r.amount}</div>
+            {/* {day(r.createdTime).format("YYYY年MM月DD日")} */}
+          </Item>
+        ))}
     </Layout>
   );
 }
